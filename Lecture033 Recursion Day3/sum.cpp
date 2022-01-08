e27abcea7266e545d38806c09de9930557798d84
08Ujjwal <78610339+08Ujjwal@users.noreply.github.com>
2022-01-08 17:00:09 +0530

    Update sum.cpp

--- Lecture033 Recursion Day3/sum.cpp
+++ Lecture033 Recursion Day3/sum.cpp
@@ -1,30 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+#include<math.h>
 
-int getSum(int *arr, int size) {
-
-    //base case
-    if(size == 0) {
-        return 0;
-    }
-    if(size == 1 )
-    {
-        return arr[0];
-    }
-
-    int remainingPart = getSum(arr+1, size-1);
-    int sum = arr[0] + remainingPart;
-    return sum;
+int  gsum(int arr[],int size){
+  if(size==0){
+      return 0;
+  }
+  int i=0;
+  return arr[i]+gsum(arr+1,size-1);
+  i++;
 }
+    
 
-int main() {
+int main(){
+  
+  int arr[5]={1,4,2,5,6};
+  int size=5;
+  int sum=gsum(arr,size);
+  cout<<sum;
 
-    int arr[5] = {2,4,9,9,9};
-    int size = 5;
-
-    int sum = getSum(arr, size);
-
-    cout << "Sum is " << sum << endl;
-
-    return 0;
 }
